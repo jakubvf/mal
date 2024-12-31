38c416c4900cbefab2f4be46240e995386a076bf
Jakub Václav Flasar <jakub@flasar.net>
2024-12-31 11:56:17 +0100

    step0: use input from GNU readline

--- impls/zig/build.zig
+++ impls/zig/build.zig
@@ -4,12 +4,9 @@ pub fn build(b: *Builder) void {
 
     //  Two options select the built step.
 
-    const name = b.option([]const u8, "name", "step name (without .zig)")
-                 orelse "stepA_mal";
+    const name = b.option([]const u8, "name", "step name (without .zig)") orelse "stepA_mal";
 
-    const root_source_file = b.path(
-        b.option([]const u8, "root_source_file", "step name (with .zig)")
-        orelse "stepA_mal.zig");
+    const root_source_file = b.path(b.option([]const u8, "root_source_file", "step name (with .zig)") orelse "stepA_mal.zig");
 
     const exe = b.addExecutable(.{
         .name = name,
@@ -18,7 +15,7 @@ pub fn build(b: *Builder) void {
         .optimize = b.standardOptimizeOption(.{}),
     });
 
-    exe.linkSystemLibrary("c");
+    exe.linkLibC();
     exe.linkSystemLibrary("pcre");
     exe.linkSystemLibrary("readline");
     b.default_step.dependOn(&exe.step);
--- impls/zig/step0_repl.zig
+++ impls/zig/step0_repl.zig
@@ -1,33 +1,32 @@
 const std = @import("std");
+const readline = @cImport(@cInclude("readline/readline.h"));
 
-
-
-pub fn READ(read_buffer: []u8) ![]const u8 {
-    const in = std.io.getStdIn();
-
-    _ = PRINT("\nuser> ") catch @panic("Hell let lose");
-    return try in.reader().readUntilDelimiter(read_buffer, '\n');
+/// Result should be freed
+pub fn READ(str: []const u8) []const u8 {
+    return str;
 }
 
 pub fn EVAL(str: []const u8) []const u8 {
     return str;
 }
 
-pub fn PRINT(str: []const u8) ![]const u8 {
-    const out = std.io.getStdOut();
-    try out.writeAll(str);
+pub fn PRINT(str: []const u8) []const u8 {
     return str;
 }
 
-pub fn rep() !void {
-    const read_buffer_size = 1024;
-
-    var read_buffer: [read_buffer_size]u8 = [_]u8{0} ** read_buffer_size;
-    _ = try PRINT(EVAL(try READ(&read_buffer)));
+pub fn rep(str: []const u8) []const u8 {
+    const result = PRINT(EVAL(READ(str)));
+    return result;
 }
 
 pub fn main() !void {
-    while (true) {
-        try rep();
+    while (readline.readline("user> ")) |input| {
+        const span = std.mem.span(input);
+        const result = rep(span);
+        const stdout = std.io.getStdOut();
+
+        try stdout.writeAll(result);
+        try stdout.writeAll("\n");
+        std.heap.raw_c_allocator.free(result);
     }
 }
